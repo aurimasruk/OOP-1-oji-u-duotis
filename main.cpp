7408bd88380caddb8da226134c04153e5f981c07
aurimasruk <aurimas.ruksys@gmail.com>
2021-02-14 22:24:59 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,8 +1,30 @@
-#include<iostream>
+#include <iostream>
 
 using namespace std;
 
+int isvedimas(int l, string name, string mystr){
+	for(int i = -2; i < l; i++)	cout << "*"; cout << endl << "*";
+
+	for(int i = 0; i < l; i++)	cout << " "; cout << "*" << endl;
+	
+	cout << "* " << mystr << name << " *" << endl << "*";
+	
+	for(int i = 0; i < l; i++)	cout << " "; cout << "*" << endl;
+
+	for(int i = -2; i < l; i++)	cout << "*";
+}
+
 int main(){
 
-return 0;
+	int l;
+	string name, mystr = "Sveikas, ";
+
+	cout << "Enter your name: ";
+	cin >> name;
+
+	l = mystr.length() + name.length() + 2;
+
+	isvedimas(l, name, mystr);
+
+	return 0;
 }
